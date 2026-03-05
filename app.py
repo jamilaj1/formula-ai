12ce5a2afed9c3de7e4f7fe62048fd8717ec4fdd
jamilaj1 <jamilaj1@gmail.com>
2026-03-05 10:29:46 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import google.generativeai as genai
+from supabase import create_client
+
+st.set_page_config(page_title="Formula AI", layout="wide")
+
+# secrets
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+API_KEY = st.secrets["API_KEY"]
+
+# connect supabase
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+# connect gemini
+genai.configure(api_key=API_KEY)
+
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
+
+st.title("Industrial Formula AI")
+
+product = st.selectbox(
+    "Product Type",
+    ["Detergent","Shampoo","Paint","Adhesive","Cosmetics"]
+)
+
+requirements = st.text_area("Describe your product")
+
+if st.button("Generate Formula"):
+
+    prompt = f"""
+You are a senior chemical engineer.
+
+Generate industrial formula.
+
+Product:
+{product}
+
+Requirements:
+{requirements}
+
+Return:
+
+Ingredients
+Percentages
+Manufacturing process
+Safety notes
+"""
+
+    response = model.generate_content(prompt)
+
+    st.markdown(response.text)
